532dfbf266b9b584f352f2e0c549d827ca061652
aaduru <ujwala_aaduru@yahoo.com>
2017-01-01 14:00:08 -0800

    Phase 2 completed

--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 import Root from './components/root';
 
-window.Note;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
 	const root = document.getElementById("root");
-	ReactDOM.render(<Root/>, root);
+	ReactDOM.render(<h1> Synthesizer</h1>, root);
+
+  window.store = store;
 });
